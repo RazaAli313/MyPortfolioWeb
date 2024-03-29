6f9e87cc8b854c20f29cc5456184a10b932dfff7
Raza Ali <136004622+RazaAli313@users.noreply.github.com>
2024-03-29 16:12:35 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Syed Muhammad Raza Ali",
+  title: "Web Developer & Graphic Designer",
+  email: "razaalipk313@gmail.com",
+  gitHub: "https://github.com/RazaAli313",
+  instagram: "https://www.instagram.com/muhammadrazaali313?igsh=YzljYTk1ODg3Zg==",
+  linkedIn: "https://www.linkedin.com/in/muhammad-raza-ali-748425256/",
+  // medium: "",
+  twitter: "https://x.com/razaalipk313?t=S-jrM9XNB0tdiuYgAKLX6Q&s=08",
+  youTube: "https://www.youtube.com/@maktabeshaheen5347",
 };
 
 const primaryColor = "#4E567E";
